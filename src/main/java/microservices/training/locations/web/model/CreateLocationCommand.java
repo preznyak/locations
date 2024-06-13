968cbdeff17bf4d6c1dc88a3589a5d7da813fcb6
Laszlo Preznyak <laszlopreznyak11@gmail.com>
2024-06-13 14:56:58 +0200

    WebMvc Tests.

--- src/main/java/microservices/training/locations/web/model/CreateLocationCommand.java
+++ src/main/java/microservices/training/locations/web/model/CreateLocationCommand.java
@@ -1,8 +1,12 @@
 package microservices.training.locations.web.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CreateLocationCommand {
     private String name;
     private double lat;
---
+++ src/test/java/microservices/training/locations/web/LocationsControllerWebMvcIT.java
@@ -0,0 +1,72 @@
+package microservices.training.locations.web;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import microservices.training.locations.service.LocationsService;
+import microservices.training.locations.web.model.CreateLocationCommand;
+import microservices.training.locations.web.model.LocationDto;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = LocationsController.class)
+public class LocationsControllerWebMvcIT {
+
+    @MockBean
+    LocationsService locationsService;
+
+    @Autowired
+    MockMvc mockMvc;
+
+    ObjectMapper objectMapper = new ObjectMapper();
+
+    @Test
+    void testListLocations() throws Exception {
+        when(locationsService.listLocations(any()))
+                .thenReturn(List.of(
+                        new LocationDto(1L, "Csokaly", 45.12, 20.41),
+                        new LocationDto(2L, "Szalacs", 44.98, 21.02)
+                ));
+
+        mockMvc.perform(get("/api/locations"))
+//                .andDo(print());
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].name", equalTo("Csokaly")));
+    }
+
+    @Test
+    void testFindLocationById() throws Exception {
+        when(locationsService.findLocationById(1L))
+                .thenReturn(new LocationDto(1L, "Csokaly", 45.12, 20.41));
+
+        mockMvc.perform(get("/api/locations/1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("name", equalTo("Csokaly")));
+    }
+
+    @Test
+    void testCreateLocation() throws Exception {
+
+        CreateLocationCommand command = new CreateLocationCommand("Kiskereki", 42.56, 20.12);
+        LocationDto locationDto = new LocationDto(1L, "Kiskereki", 42.56, 20.12);
+        when(locationsService.createLocation(command))
+                .thenReturn(locationDto);
+        String requestBody = objectMapper.writeValueAsString(command);
+
+        mockMvc.perform(post("/api/locations").content(requestBody).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("name", equalTo("Kiskereki")));
+    }
+}
